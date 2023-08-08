682c952c0b9633f80937e360b085f706070943fa
Max <maxim.90svet@gmail.com>
2023-08-09 00:33:26 +0300

    add gcd in math.js

--- src/math.js
+++ src/math.js
@@ -5,4 +5,14 @@ const getRandomInteger = (range = [1, Number.MAX_SAFE_INTEGER]) => {
   return min + Math.floor(Math.random() * (max - min));
 };
 
-export { isEven, getRandomInteger };
+const gcdRec = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+
+  return gcdRec(b, a % b);
+};
+
+const gcd = (a, b) => ((a < b) ? gcdRec(b, a) : gcdRec(b, a));
+
+export { isEven, getRandomInteger, gcd };
